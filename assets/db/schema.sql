4c506a3c113a7c001387e2d9cb5777afff0e9971
Wesley Slifer <wesleyslifer@ymail.com>
2020-07-31 12:00:39 -0500

    updating schema

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE employee(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
-    manager_id INT,
+    manager_id INT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(role_id) REFERENCES role(id),
     FOREIGN KEY(manager_id) REFERENCES employee(id)
